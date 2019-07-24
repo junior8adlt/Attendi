ee1abcbebd75ab42d074d2b6ce2250099a31ad88
Alberto Ochoa <alberto.ochoadltorre@gmail.com>
2019-07-23 23:01:31 -0500

    Updates

---
+++ precache-manifest.0d1d21ab41a69bd998497fbc0618f7c1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd9626751f2423c4932647457638277",
+    "url": "/Attendi/index.html"
+  },
+  {
+    "revision": "8f31817ad2f95a40297e",
+    "url": "/Attendi/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b6dc761fbe9e55612521",
+    "url": "/Attendi/static/css/main.058d0df4.chunk.css"
+  },
+  {
+    "revision": "8f31817ad2f95a40297e",
+    "url": "/Attendi/static/js/2.384b2fd0.chunk.js"
+  },
+  {
+    "revision": "b6dc761fbe9e55612521",
+    "url": "/Attendi/static/js/main.b20ae985.chunk.js"
+  },
+  {
+    "revision": "3485a3e65530efe0c8b2",
+    "url": "/Attendi/static/js/runtime~main.192a3f49.js"
+  },
+  {
+    "revision": "b7532bcfc90422f1e1cb7d9311f4da5f",
+    "url": "/Attendi/static/media/Logo Attendi_movil.b7532bcf.png"
+  },
+  {
+    "revision": "037ef6c216bd53bcaf72419b7fb9f37e",
+    "url": "/Attendi/static/media/escala_1_mov.037ef6c2.png"
+  },
+  {
+    "revision": "f8a779fbc01a207915da71c85b038754",
+    "url": "/Attendi/static/media/escala_3_mov.f8a779fb.png"
+  },
+  {
+    "revision": "ab9f9ea3d7e0d759c7bd3687e1e4bfcc",
+    "url": "/Attendi/static/media/escala_5_mov.ab9f9ea3.png"
+  },
+  {
+    "revision": "4b1fcf2c5c3d2a3503fe799a11a2754e",
+    "url": "/Attendi/static/media/escala_6_mov.4b1fcf2c.png"
+  },
+  {
+    "revision": "070a9269082474ecd06b6d64ebd0fa2f",
+    "url": "/Attendi/static/media/glacial-indifference.bold.070a9269.otf"
+  },
+  {
+    "revision": "008080d5594fd00507fc8a2c93443d39",
+    "url": "/Attendi/static/media/glacial-indifference.regular.008080d5.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/attendi/precache-manifest.6169c465e491812e89caac4471daaca5.js"
+  "/Attendi/precache-manifest.0d1d21ab41a69bd998497fbc0618f7c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/attendi/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Attendi/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
